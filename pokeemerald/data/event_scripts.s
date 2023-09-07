f631efbf569e36113889617049619d1aecc9bc08
DustinJoosen <dustinjoosen2003@gmail.com>
2023-09-07 21:46:59 +0200

    Fix map

--- pokeemerald/data/event_scripts.s
+++ pokeemerald/data/event_scripts.s
@@ -1055,7 +1055,3 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
-
-	.include "data/maps/FirstGymTestMap/scripts.inc"
-
-	.include "data/maps/LitttlerootPokecenter/scripts.inc"
