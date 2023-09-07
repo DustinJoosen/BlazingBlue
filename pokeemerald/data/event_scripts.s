405f6c838113774f98d985a2bc8c0274922e8cc3
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2023-09-07 20:26:22 +0200

    Automate adding pokemon (#3)
    * Add porysript compiler
    
    * update the sprites
    
    * Add testing gym
    
    * Finish auto-mon-adder.py

--- pokeemerald/data/event_scripts.s
+++ pokeemerald/data/event_scripts.s
@@ -1055,3 +1055,7 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/FirstGymTestMap/scripts.inc"
+
+	.include "data/maps/LitttlerootPokecenter/scripts.inc"
